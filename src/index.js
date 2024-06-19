2eee6871426d3c530f08e4f21678f5589aed12bc
3Tarun <tarun.h@rovelabs.com>
2024-06-19 14:06:18 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/index.css';
+import './assets/styles/bootstrap.custom.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './Screens/HomeScreen';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter(
+  createRoutesFromElements([
+    <Route path="/" element={<App/>}> 
+      <Route index={true} path="/" element={<HomeScreen/>}/>
+    </Route>
+  ])
+)
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
